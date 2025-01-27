236b7e8e75e3cd449d96433ed5966da76ebf9a13
Paulo <paulosouzap7@gmail.com>
2025-01-26 22:48:22 -0300

    feat: adjusment eslint cfg

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,19 +1,19 @@
-import "dotenv/config";
+import 'dotenv/config'
 
-import { preprocess, z } from "zod";
+import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z
-    .enum(["development", "production", "test"])
-    .default("development"),
+    .enum(['development', 'production', 'test'])
+    .default('development'),
   PORT: z.coerce.number().default(3000),
-});
+})
 
-const _env = envSchema.safeParse(process.env);
+const _env = envSchema.safeParse(process.env)
 
 if (!_env.success) {
-  console.error("Invalid environment variables", _env.error.format());
-  throw new Error("Invalid environment variables");
+  console.error('Invalid environment variables', _env.error.format())
+  throw new Error('Invalid environment variables')
 }
 
-export const env = _env.data;
+export const env = _env.data
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import { app } from "./app";
-import { env } from "./env";
+// eslint-disable-next-line prettier/prettier
+import { app } from './app'
+import { env } from './env'
 app
   .listen({
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: env.PORT,
   })
   .then(() => {
-    console.log("Servidor is running on http://localhost:3000");
-  });
+    console.log('Servidor is running on http://localhost:3000')
+  })

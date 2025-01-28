3b6d8770a9f23361a58785deed9e85feb6cd368c
Paulo <paulosouzap7@gmail.com>
2025-01-27 22:59:10 -0300

    feat: creation create controller

---
+++ src/http/controllers/person/create.ts
@@ -0,0 +1,28 @@
+import { PersonRepository } from '@/repositories/person.repository'
+import { CreatePersonUseCase } from '@/use-cases/create-person'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    cpf: z.string(),
+    name: z.string(),
+    birth: z.date(),
+    email: z.string().email(),
+  })
+
+  const { birth, cpf, email, name } = registerBodySchema.parse(request.body)
+
+  try {
+    const personRepository = new PersonRepository()
+    const createPersonUseCase = new CreatePersonUseCase(personRepository)
+
+    await createPersonUseCase.handler({ cpf, birth, email, name })
+
+    return reply.status(201).send()
+  } catch (error) {
+    console.error(error)
+
+    throw new Error('Internal Server Error')
+  }
+}

c08abcd22b47cb7327c1c6fda0cafdd1a50b0785
Paulo <paulosouzap7@gmail.com>
2025-02-04 22:11:37 -0300

    feat: product and category mapping using typeorm

--- src/lib/typeorm/typeorm.ts
+++ src/lib/typeorm/typeorm.ts
@@ -2,6 +2,8 @@ import { DataSource } from 'typeorm'
 
 import { env } from '@/env'
 import { error } from 'console'
+import { Product } from '@/entities/product.entity'
+import { Category } from '@/entities/category.entity'
 
 export const appDataSource = new DataSource({
   type: 'postgres',
@@ -10,7 +12,7 @@ export const appDataSource = new DataSource({
   username: env.DATABASE_USER,
   password: env.DATABASE_PASSWORD,
   database: env.DATABASE_NAME,
-  entities: [],
+  entities: [Product, Category],
   logging: env.NODE_ENV === 'development',
 })
 

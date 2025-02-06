40111b5bd1e14132b6118dd866f3754b572efd9a
Paulo <paulosouzap7@gmail.com>
2025-02-05 22:15:12 -0300

    feat: add migration with UUID in product

--- src/lib/typeorm/typeorm.ts
+++ src/lib/typeorm/typeorm.ts
@@ -4,6 +4,7 @@ import { env } from '@/env'
 import { error } from 'console'
 import { Product } from '@/entities/product.entity'
 import { Category } from '@/entities/category.entity'
+import { ProductAutoGenerateUUID1738803666034 } from './migrations/1738803666034-ProductAutoGenerateUUID'
 
 export const appDataSource = new DataSource({
   type: 'postgres',
@@ -13,6 +14,7 @@ export const appDataSource = new DataSource({
   password: env.DATABASE_PASSWORD,
   database: env.DATABASE_NAME,
   entities: [Product, Category],
+  migrations: [ProductAutoGenerateUUID1738803666034],
   logging: env.NODE_ENV === 'development',
 })
 

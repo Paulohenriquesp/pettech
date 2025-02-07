39ff344c699f36b819ce1d0780bfd4e6d8b6f559
Paulo <paulosouzap7@gmail.com>
2025-02-07 20:33:49 -0300

    feat: creating findall/findproduct/updateproduct useCases

---
+++ src/use-cases/find-products.ts
@@ -0,0 +1,15 @@
+import { IProductRepository } from '@/repositories/product.repository.interface'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+export class FindProductUseCase {
+  constructor(private productRepository: IProductRepository) {
+    this.productRepository = productRepository
+  }
+
+  async handler(id: string) {
+    const product = await this.productRepository.findById(id)
+
+    if (!product) throw new ResourceNotFoundError()
+    return product
+  }
+}
